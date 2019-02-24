282f436533cfa913f86d4b1cded6f3307d1cc430
Ryan McKern <ryan@mckern.sh>
2019-02-24 02:32:58 -0800

    Add a string character deduplication method!
    Neat! Academically rigorous!

--- useful.rb
+++ useful.rb
@@ -12,6 +12,19 @@ def two_one?
   Gem::Version.new(RUBY_VERSION) <= Gem::Version.new("2.1.0")
 end
 
+def dedupe(str)
+  if str.empty?
+    str.split('').each_with_index do |char, i|
+      if str[i] == str[i + 1]
+        str.slice!(i + 1)
+        return dedupe(str)
+      end
+    end
+  else
+    return false
+  end
+end
+
 module UniqueMethodExtension
   def unique_methods
     m = [methods - Object.methods].flatten.sort
